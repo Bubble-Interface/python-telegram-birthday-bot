ffed5d09c9d74e5e49bc0afb1c06f2c47d129632
Michael_Interface <interfacemichael@gmail.com>
2023-12-01 20:54:25 +0300

    13 (feat): preparing deploy

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,8 @@ WORKDIR /app
 
 ENV PYTHONUNBUFFERED 1
 
-COPY requirements.txt requirements.txt
-COPY birthday_bot.py birthday_bot.py
-COPY service service
-COPY models.py models.py
-
+COPY . .
 
 RUN pip3 install -r requirements.txt
 
-CMD [ "python", "birthday_bot.py"]
+CMD [ "python", "main.py"]
--- db/__init__.py
+++ db/__init__.py
@@ -17,12 +17,11 @@ db_host = os.environ.get("DB_HOST", 'birthday_bot_postgres')
 db_port = os.environ.get("DB_PORT", '5432')
 db_name = os.environ.get("POSTGRES_DB", 'birthday')
 
-# db_string = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
-url = f'postgresql://{db_user}:{db_pass}@localhost:{db_port}/{db_name}'
+db_string = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
 
-if not database_exists(url):
-    create_database(url)
-engine = create_engine(url)
+if not database_exists(db_string):
+    create_database(db_string)
+engine = create_engine(db_string)
 
 Session = sessionmaker(engine)
 Base.metadata.create_all(engine)
